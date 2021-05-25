3e8fe431096fda9bb5adb38cf5cee0920d0484bc
Raja D <rajad@personanutrition.com>
2021-05-25 16:18:23 +0530

    Added Logs

--- AttentiveSftpWeekly/Program.cs
+++ AttentiveSftpWeekly/Program.cs
@@ -15,41 +15,50 @@ namespace SFTPBatchJobs
     {
         static void Main(string[] args)
         {
-            var dbHelper = new DBHelper();
-            if (ConfigurationManager.AppSettings["sftphost"]?.ToLower()?.Contains("attentive") ?? false)
+            LogHelper.Log("SFTP Batch Started");
+            try
             {
-                var dataset = dbHelper.GetDataSet(Constants.Attentive_weekly_Job);
-                var filedate = DateTime.Now.ToString("ddMMyyyy");
-                if (dataset.Tables[0].Rows.Count > 0)
-                    SFTPHelper.FileUploadSFTP(dataset.Tables[0], $"PurchaserCustomers_{filedate}.csv");
-                if (dataset.Tables[1].Rows.Count > 0)
-                    SFTPHelper.FileUploadSFTP(dataset.Tables[1], $"CancelledCustomers_{filedate}.csv");
-            }
-            else if (ConfigurationManager.AppSettings["sftphost"]?.ToLower()?.Contains("40.86.85.18") ?? false)
-            {
-                var filedate = DateTime.Now.ToString("yyyyMMddHH");
-                var dataset = dbHelper.GetDataSet(Constants.WunderKind_Hourly_Job, false);
-                if (dataset.Tables[0].Rows.Count > 0)
-                    SFTPHelper.FileUploadSFTP(dataset.Tables[0], $"Persona_Subscribers_{filedate}.csv");
-
-                if (ConfigurationManager.AppSettings["BrazeApiKey"] != null)
+                var dbHelper = new DBHelper();
+                if (ConfigurationManager.AppSettings["sftphost"]?.ToLower()?.Contains("attentive") ?? false)
                 {
-                    var wunderKindHistory = dbHelper.GetDataSet(Constants.WunderKind_get_History, false).Tables[0].AsEnumerable()
-                               .Select(r => r.Field<string>("FileName"))
-                               .ToList();
+                    var dataset = dbHelper.GetDataSet(Constants.Attentive_weekly_Job);
+                    var filedate = DateTime.Now.ToString("ddMMyyyy");
+                    if (dataset.Tables[0].Rows.Count > 0)
+                        SFTPHelper.FileUploadSFTP(dataset.Tables[0], $"PurchaserCustomers_{filedate}.csv");
+                    if (dataset.Tables[1].Rows.Count > 0)
+                        SFTPHelper.FileUploadSFTP(dataset.Tables[1], $"CancelledCustomers_{filedate}.csv");
+                }
+                else if (ConfigurationManager.AppSettings["sftphost"]?.ToLower()?.Contains("40.86.85.18") ?? false)
+                {
+                    var filedate = DateTime.Now.ToString("yyyyMMddHH");
+                    var dataset = dbHelper.GetDataSet(Constants.WunderKind_Hourly_Job, false);
+                    if (dataset.Tables[0].Rows.Count > 0)
+                        SFTPHelper.FileUploadSFTP(dataset.Tables[0], $"Persona_Subscribers_{filedate}.csv");
 
-                    var processedFiles = new List<string>();
-                    var emails = SFTPHelper.GetWunderKindSFTPFiles(wunderKindHistory, out processedFiles);
-
-                    dbHelper.Insert(processedFiles.ConvertListToDataTable("FileName"), "TBL_WUNDERKIND_SUBSCRIBERS_BATCHJOB_LOG");
-                    var brazeData = new BrazeUser
+                    if (ConfigurationManager.AppSettings["BrazeApiKey"] != null)
                     {
-                        Attributes = dbHelper.GetDataSet(string.Format(Constants.WunderKind_bulk_insert, string.Join("|", emails)), false).Tables[0].ConvertToList<Utilities.Attribute>()
-                    };
-                    if (brazeData != null && brazeData.Attributes.Any())
-                        AddUserstoBraze(brazeData).GetAwaiter().GetResult();
+                        var wunderKindHistory = dbHelper.GetDataSet(Constants.WunderKind_get_History, false).Tables[0].AsEnumerable()
+                                   .Select(r => r.Field<string>("FileName"))
+                                   .ToList();
+
+                        var processedFiles = new List<string>();
+                        var emails = SFTPHelper.GetWunderKindSFTPFiles(wunderKindHistory, out processedFiles);
+
+                        dbHelper.Insert(processedFiles.ConvertListToDataTable("FileName"), "TBL_WUNDERKIND_SUBSCRIBERS_BATCHJOB_LOG");
+                        var brazeData = new BrazeUser
+                        {
+                            Attributes = dbHelper.GetDataSet(string.Format(Constants.WunderKind_bulk_insert, string.Join("|", emails)), false).Tables[0].ConvertToList<Utilities.Attribute>()
+                        };
+                        if (brazeData != null && brazeData.Attributes.Any())
+                            AddUserstoBraze(brazeData).GetAwaiter().GetResult();
+                    }
                 }
             }
+            catch(Exception ex)
+            {
+                LogHelper.Error(ex, "Error in SFTP batch job");
+            }
+            LogHelper.Log("SFTP Batch Ended");
         }
 
 
@@ -57,16 +66,25 @@ namespace SFTPBatchJobs
 
         private static async Task<string> AddUserstoBraze(BrazeUser data)
         {
-
-            var client = new HttpClient();
-            var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
-            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {ConfigurationManager.AppSettings["BrazeApiKey"]}");
-            using (var response = await client.PostAsync("https://rest.iad-02.braze.com/users/track", content).ConfigureAwait(false))
+            LogHelper.Log("AddUserstoBraze started");
+            try
             {
-                response.EnsureSuccessStatusCode();
-                var repsponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                return repsponse;
+                var client = new HttpClient();
+                var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {ConfigurationManager.AppSettings["BrazeApiKey"]}");
+                using (var response = await client.PostAsync("https://rest.iad-02.braze.com/users/track", content).ConfigureAwait(false))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var repsponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return repsponse;
+                }
             }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex,"Error AddUserstoBraze ended");
+
+            }
+            LogHelper.Log("AddUserstoBraze ended");
             return null;
         }
     }
---
+++ Utilities/LogHelper.cs
@@ -0,0 +1,31 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class LogHelper
+    {
+        private static ILog logger;
+        static LogHelper()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            logger = log4net.LogManager.GetLogger("Persona Batch Job");
+
+        }
+
+        public static void Log(string message)
+        {
+            logger.Info(message);
+        }
+
+        public static void Error(Exception ex, string message = "")
+        {
+            logger.Error(message + ex.Message, ex);
+        }
+
+    }
+}
