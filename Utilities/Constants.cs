98150a2c35468b39d70fda94514ce1ca626c87b9
Raja D <rajad@personanutrition.com>
2021-06-01 12:21:37 +0530

    Added WunderkindSFTPUnsubscribersDaily batch job

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -14,6 +14,16 @@ namespace Utilities
                                                     original_customer_id not in (select distinct Customer_ID from VW_INVALID_CUSTOMERS where customer_id is not null )
                                                     and original_customer_id not in (select distinct Customer_ID from TBL_DO_NOT_CALL_LIST where customer_id is not null)
 													and braze_date_added  >= DATEADD(HH, -1, GETDATE());";
+        public const string WunderKind_Unsub_Daily_Job = @";WITH CTE_GET_VALID_TRANSACTION_CUSTOMERS AS
+                                                        (SELECT  CT.TRANSACTION_CUSTOMER_ID FROM TBL_CUSTOMER_TRANSACTION  CT WITH (NOLOCK)
+                                                               WHERE (CT.TRANSACTION_STATUS IN ( 3, 5, 8 )  OR ( CT.TRANSACTION_STATUS = 6  AND ( CT.DOCKET_NUMBER IS NOT NULL  OR CT.SHIP_DATE IS NOT NULL ))))
+                                                        , CTE_AUTOSHIP_PAUSE_DATA AS 
+                                                        (SELECT DISTINCT SO.TRANSACTION_CUSTOMER_ID, CAST(MAX(SOASS.CREADTED_DATE) AS DATE) AS AUTOSHIP_PAUSE_DATE FROM TBL_STANDING_ORDERS SO  WITH (NOLOCK) 
+                                                               JOIN TBL_STANDING_ORDER_AUTO_SHIP_STATUS SOASS  WITH (NOLOCK) ON SO.STANDING_ORDER_ID = SOASS.STANDING_ORDER_ID WHERE SO.AUTO_SHIP = 4 GROUP BY SO.TRANSACTION_CUSTOMER_ID)
+                                                        SELECT DISTINCT CL.FIRST_NAME,CL.LAST_NAME,CL.EMAIL,CTE.AUTOSHIP_PAUSE_DATE UnSubscribedDate  FROM TBL_CUSTOMER_LIST CL WITH (NOLOCK) 
+                                                        JOIN CTE_AUTOSHIP_PAUSE_DATA CTE ON CL.CUSTOMER_ID = CTE.TRANSACTION_CUSTOMER_ID JOIN CTE_GET_VALID_TRANSACTION_CUSTOMERS CT ON CT.TRANSACTION_CUSTOMER_ID = CTE.TRANSACTION_CUSTOMER_ID
+                                                        WHERE CTE.AUTOSHIP_PAUSE_DATE >= Getdate()-2 and CTE.AUTOSHIP_PAUSE_DATE < Getdate()-1 AND NOT EXISTS (SELECT CL.CUSTOMER_ID FROM VW_INVALID_CUSTOMERS CL WHERE CL.CUSTOMER_ID= CTE.TRANSACTION_CUSTOMER_ID) 
+                                                        AND NOT EXISTS ( SELECT DSE.CUSTOMER_ID FROM TBL_DO_NOT_SEND_EMAIL_LIST DSE WHERE DSE.CUSTOMER_ID = CTE.TRANSACTION_CUSTOMER_ID ) ";
         public const string WunderKind_get_History = @"select [FileName] from [TBL_WUNDERKIND_SUBSCRIBERS_BATCHJOB_LOG]";
 
 
---
+++ WunderkindSFTPUnsubscribersDaily/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace WunderkindSFTPUnsubscribersDaily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbHelper = new DBHelper();
+            var filedate = DateTime.Now.ToString("yyyyMMdd");
+            var dataset = dbHelper.GetDataSet(Constants.WunderKind_Unsub_Daily_Job, false);
+            if (dataset.Tables[0].Rows.Count > 0)
+                SFTPHelper.FileUploadSFTP(dataset.Tables[0], $"Persona_UnSubscribers_{filedate}.csv");
+        }
+    }
+}
